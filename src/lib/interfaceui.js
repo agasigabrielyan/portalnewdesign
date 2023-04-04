04ad4d5703120991d1883845906e5c9696874f78
agasi <agasi79@list.ru>
2023-04-04 18:12:01 +0300

    Закоммитить

--- src/lib/interfaceui.js
+++ src/lib/interfaceui.js
@@ -172,6 +172,7 @@ class InterfaceUI {
         return widgetSubMenu;
     }
 
+    // создание одного элемента
     createSubmenuSingleItem(litem) {
         let litemHtml = "";
 
--- src/lib/script.js
+++ src/lib/script.js
@@ -1,15 +1,15 @@
 window.onload = function() {
     let simple = [
-        {x: 0, y: 0, w: 2, h: 1, content: '<div><div>1</div><div>Совместная работа</div></div>'},
-        {x: 2, y: 0, w: 1, h: 2, content: '<div><div>2</div><div>Календарь</div></div>'},
-        {x: 0, y: 1, w: 2, h: 1, content: '<div><div>3</div><div>Мои услуги</div></div>'},
-        {x: 0, y: 2, w: 1, h: 1, content: '<div><div>4</div><div>САД ПАО “Газпром”</div></div>'},
-        {x: 1, y: 2, w: 1, h: 1, content: '<div><div>5</div><div>Статус АСЭЗ</div></div>'},
-        {x: 2, y: 2, w: 1, h: 1, content: '<div><div>6</div><div>ПУР АСБУ</div></div>'},
-        {x: 0, y: 3, w: 1, h: 2, content: '<div><div>7</div><div>Мои новости</div></div>'},
-        {x: 1, y: 3, w: 1, h: 1, content: '<div><div>8</div><div>Мои системы</div></div>'},
-        {x: 2, y: 3, w: 1, h: 1, content: '<div><div>9</div><div>Расписание автобусов</div></div>'},
-        {x: 1, y: 4, w: 2, h: 1, content: '<div><div>10</div><div>Мои запросы в службу поддержки</div></div>'},
+        {x: 0, y: 0, w: 2, h: 1, content: '<div><div>1</div><div>Совместная работа</div></div>', datainfo: ''},
+        {x: 2, y: 0, w: 1, h: 2, content: '<div><div>2</div><div>Календарь</div></div>', datainfo: ''},
+        {x: 0, y: 1, w: 2, h: 1, content: '<div><div>3</div><div>Мои услуги</div></div>', datainfo: ''},
+        {x: 0, y: 2, w: 1, h: 1, content: '<div><div>4</div><div>САД ПАО “Газпром”</div></div>', datainfo: ''},
+        {x: 1, y: 2, w: 1, h: 1, content: '<div><div>5</div><div>Статус АСЭЗ</div></div>', datainfo: ''},
+        {x: 2, y: 2, w: 1, h: 1, content: '<div><div>6</div><div>ПУР АСБУ</div></div>', datainfo: ''},
+        {x: 0, y: 3, w: 1, h: 2, content: '<div><div>7</div><div>Мои новости</div></div>', datainfo: ''},
+        {x: 1, y: 3, w: 1, h: 1, content: '<div><div>8</div><div>Мои системы</div></div>', datainfo: ''},
+        {x: 2, y: 3, w: 1, h: 1, content: '<div><div>9</div><div>Расписание автобусов</div></div>', datainfo: ''},
+        {x: 1, y: 4, w: 2, h: 1, content: '<div><div>10</div><div>Мои запросы в службу поддержки</div></div>', datainfo: ''},
     ];
 
     let simpleGrid = GridStack.init({
